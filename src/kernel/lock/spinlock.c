c0f88a93143feb52b8b90876278e2ba09cea89f6
KenoCarte <3525165133@qq.com>
2026-06-15 20:22:08 +0800

    lab-1: 基本实现函数

--- src/kernel/lock/spinlock.c
+++ src/kernel/lock/spinlock.c
@@ -10,20 +10,18 @@
 */
 
 // 带层数叠加的关中断
-void push_off(void)
-{
+void push_off(void) {
     int old = intr_get();
     intr_off();
-    cpu_t *cpu = mycpu();
+    cpu_t* cpu = mycpu();
     if (cpu->noff == 0)
         cpu->origin = old;
     cpu->noff++;
 }
 
 // 带层数叠加的开中断
-void pop_off(void)
-{
-    cpu_t *cpu = mycpu();
+void pop_off(void) {
+    cpu_t* cpu = mycpu();
     assert(intr_get() == 0, "push_off: 1\n"); // 确保此时中断是关闭的
     assert(cpu->noff >= 1, "push_off: 2\n");  // 确保push和pop的对应
     cpu->noff--;
@@ -33,25 +31,31 @@ void pop_off(void)
 
 
 // 自选锁初始化
-void spinlock_init(spinlock_t *lk, char *name)
-{
-
+void spinlock_init(spinlock_t* lk, char* name) {
+    lk->locked = 0;
+    lk->name = name;
+    lk->cpuid = -1;
 }
 
 // 是否持有自旋锁
-bool spinlock_holding(spinlock_t *lk)
-{
-
+bool spinlock_holding(spinlock_t* lk) {
+    return lk->locked && lk->cpuid == r_mhartid();
 }
 
 // 获取自选锁
-void spinlock_acquire(spinlock_t *lk)
-{
-
+void spinlock_acquire(spinlock_t* lk) {
+    push_off();
+    assert(!spinlock_holding(lk), "spinlock_acquire");
+    while (__sync_lock_test_and_set(&lk->locked, 1) != 0);
+    __sync_synchronize();
+    lk->cpuid = r_mhartid();
 }
 
 // 释放自旋锁
-void spinlock_release(spinlock_t *lk)
-{
-
+void spinlock_release(spinlock_t* lk) {
+    assert(spinlock_holding(lk), "spinlock_release");
+    __sync_lock_release(&lk->locked);
+    __sync_synchronize();
+    lk->cpuid = -1;
+    pop_off();
 }
