07055d3a8cf7dd28910fc23f17ba7914b5bcab08
KenoCarte <3525165133@qq.com>
2026-06-23 15:53:04 +0800

    lab-4：完成trap_user_return()
    Signed-off-by: KenoCarte <3525165133@qq.com>

--- src/kernel/trap/trap_user.c
+++ src/kernel/trap/trap_user.c
@@ -9,19 +9,27 @@ extern char user_return[]; // 内核处理完毕返回用户
 extern char kernel_vector[]; // 内核态trap处理流程, 进入内核后应当切换中断处理入口
 
 // in trap_kernel.c
-extern char *interrupt_info[16]; // 中断错误信息
-extern char *exception_info[16]; // 异常错误信息
+extern char* interrupt_info[16]; // 中断错误信息
+extern char* exception_info[16]; // 异常错误信息
 
 // 在user_vector()里面调用
 // 用户态trap处理的核心逻辑
-void trap_user_handler()
-{
+void trap_user_handler() {
 
 }
 
 // 调用user_return()
 // 内核态返回用户态
-void trap_user_return()
-{
-
+void trap_user_return() {
+    proc_t* p = mycpu()->proc;
+    assert(p != NULL, "trap_user_handler: p is NULL");
+    p->tf->user_to_kern_hartid = mycpuid();
+    p->tf->user_to_kern_sp = (uint64)p->kstack + PGSIZE;
+    p->tf->user_to_kern_trapvector = (uint64)user_vector;
+    w_sepc(p->tf->user_to_kern_epc);
+    uint64 status = r_sstatus();
+    status &= ~SSTATUS_SPP;
+    status |= SSTATUS_SPIE;
+    w_sstatus(status);
+    user_return(p->tf, MAKE_SATP(p->pagetable));
 }
