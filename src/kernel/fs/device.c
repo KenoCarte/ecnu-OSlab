d4ba3f346b56ee82c151ff21a4c7d3565d484dd8
KenoCarte <3525165133@qq.com>
2026-06-30 15:58:01 +0800

    lab-9: 完成fs.c

--- src/kernel/fs/device.c
+++ src/kernel/fs/device.c
@@ -3,36 +3,31 @@
 device_t device_table[N_DEVICE];
 
 /* 标准输入设备 */
-static uint32 device_stdin_read(uint32 len, uint64 dst, bool is_user_dst)
-{
+static uint32 device_stdin_read(uint32 len, uint64 dst, bool is_user_dst) {
 	return cons_read(len, dst, is_user_dst);
 }
 
 /* 标准输出设备 */
-static uint32 device_stdout_write(uint32 len, uint64 src, bool is_user_src)
-{
+static uint32 device_stdout_write(uint32 len, uint64 src, bool is_user_src) {
 	return cons_write(len, src, is_user_src);
 }
 
 /* 标准错误输出设备 */
-static uint32 device_stderr_write(uint32 len, uint64 src, bool is_user_src)
-{
+static uint32 device_stderr_write(uint32 len, uint64 src, bool is_user_src) {
 	printf("ERROR: ");
 	return cons_write(len, src, is_user_src);
 }
 
 /* 无限0流 */
-static uint32 device_zero_read(uint32 len, uint64 dst, bool is_user_dst)
-{
+static uint32 device_zero_read(uint32 len, uint64 dst, bool is_user_dst) {
 	uint32 write_len = 0, cut_len = 0;
 
 	uint64 src = (uint64)pmem_alloc(true);
-	proc_t *p = myproc();
+	proc_t* p = myproc();
 
-	while (write_len < len)
-	{
+	while (write_len < len) {
 		cut_len = MIN(len - write_len, PGSIZE);
-		
+
 		if (is_user_dst)
 			uvm_copyout(p->pgtbl, dst, src, cut_len);
 		else
@@ -48,22 +43,19 @@ static uint32 device_zero_read(uint32 len, uint64 dst, bool is_user_dst)
 }
 
 /* 空设备读取 */
-static uint32 device_null_read(uint32 len, uint64 dst, bool is_user_dst)
-{
+static uint32 device_null_read(uint32 len, uint64 dst, bool is_user_dst) {
 	return 0;
 }
 
 /* 空设备写入 */
-static uint32 device_null_write(uint32 len, uint64 src, bool is_user_src)
-{
+static uint32 device_null_write(uint32 len, uint64 src, bool is_user_src) {
 	return len;
 }
 
 /* 彩蛋: 笨蛋GPT */
-static uint32 device_gpt0_write(uint32 len, uint64 src, bool is_user_src)
-{
+static uint32 device_gpt0_write(uint32 len, uint64 src, bool is_user_src) {
 	char tmp[STR_MAXLEN + 1];
-	proc_t *p = myproc();
+	proc_t* p = myproc();
 
 	tmp[len] = '\0';
 
@@ -74,16 +66,20 @@ static uint32 device_gpt0_write(uint32 len, uint64 src, bool is_user_src)
 
 	if (strncmp(tmp, "Hello", len) == 0) {
 		printf("Hi, I am gpt0!\n");
-	} else if (strncmp(tmp, "Guess who I am", len) == 0) {
+	}
+	else if (strncmp(tmp, "Guess who I am", len) == 0) {
 		printf("Your procid is %d and name is %s.\n", p->pid, p->name);
-	} else if (strncmp(tmp, "How many free memory left", len) == 0) {
+	}
+	else if (strncmp(tmp, "How many free memory left", len) == 0) {
 		uint32 kernel_free_pages, user_free_pages;
 		pmem_stat(&kernel_free_pages, &user_free_pages);
 		printf("We have %d free pages in kernel space, %d free pages in user space!\n",
 			kernel_free_pages, user_free_pages);
-	} else if (strncmp(tmp, "Good job", len) == 0) {
+	}
+	else if (strncmp(tmp, "Good job", len) == 0) {
 		printf("Thanks for your kind words!\n");
-	} else {
+	}
+	else {
 		printf("Sorry, I can not understand it.\n");
 	}
 
@@ -93,33 +89,28 @@ static uint32 device_gpt0_write(uint32 len, uint64 src, bool is_user_src)
 /* 注册设备 */
 static void device_register(uint32 index, char* name,
 	uint32(*read)(uint32, uint64, bool),
-	uint32(*write)(uint32, uint64, bool))
-{
+	uint32(*write)(uint32, uint64, bool)) {
 	memmove(device_table[index].name, name, MAXLEN_FILENAME);
 	device_table[index].read = read;
 	device_table[index].write = write;
 }
 
 /* 初始化device_table */
-void device_init()
-{
+void device_init() {
 
 }
 
 /* 检查文件major字段的合法性 */
-bool device_open_check(uint16 major, uint32 open_mode)
-{
+bool device_open_check(uint16 major, uint32 open_mode) {
 
 }
 
 /* 从设备文件中读取数据 */
-uint32 device_read_data(uint16 major, uint32 len, uint64 dst, bool is_user_dst)
-{
+uint32 device_read_data(uint16 major, uint32 len, uint64 dst, bool is_user_dst) {
 
 }
 
 /* 向设备文件写入数据 */
-uint32 device_write_data(uint16 major, uint32 len, uint64 src, bool is_user_src)
-{
+uint32 device_write_data(uint16 major, uint32 len, uint64 src, bool is_user_src) {
 
 }
--- src/kernel/fs/fs.c
+++ src/kernel/fs/fs.c
@@ -32,21 +32,90 @@ file_t* file_alloc() {
 	成功返回file, 失败返回NULL
 */
 file_t* file_open(char* path, uint32 open_mode) {
-	return NULL;
+	inode_t* ip = path_to_inode(path);
+	if (ip == NULL) {
+		if (open_mode & FILE_OPEN_CREATE) {
+			ip = path_create_inode(path, INODE_TYPE_DATA, INODE_MAJOR_DEFAULT, INODE_MINOR_DEFAULT);
+			if (ip == NULL) return NULL;
+		}
+		else return NULL;
+	}
+	inode_lock(ip);
+	if (ip->disk_info.type == INODE_TYPE_DIVICE) {
+		if (!device_open_check(ip->disk_info.major, open_mode)) {
+			inode_unlock(ip);
+			inode_put(ip);
+			return NULL;
+		}
+	}
+	inode_unlock(ip);
+	file_t* file = file_alloc();
+	if (file == NULL) {
+		inode_put(ip);
+		return NULL;
+	}
+	file->ip = ip;
+	file->offset = 0;
+	file->readable = (open_mode & FILE_OPEN_READ) != 0;
+	file->writbale = (open_mode & FILE_OPEN_WRITE) != 0;
+	return file;
 }
 
 /* 关闭文件 */
 void file_close(file_t* file) {
+	if (file->ip != NULL) inode_put(file->ip);
+	file->ip = NULL;
+	spinlock_acquire(&lk_file_table);
+	file->ref--;
+	spinlock_release(&lk_file_table);
 }
 
 /* 读取文件内容, 返回读到的字节数量 */
 uint32 file_read(file_t* file, uint32 len, uint64 dst, bool is_user_dst) {
-	return 0;
+	if (file->ip == NULL || !file->readable) return 0;
+	uint32 res = 0;
+	inode_lock(file->ip);
+	switch (file->ip->disk_info.type) {
+	case INODE_TYPE_DATA:
+		res = inode_read_data(file->ip, file->offset, len, (void*)dst, is_user_dst);
+		break;
+	case INODE_TYPE_DIR:
+		res = dentry_transmit(file->ip, dst, len, is_user_dst);
+		break;
+	case INODE_TYPE_DIVICE:
+		res = device_read_data(file->ip->disk_info.major, len, dst, is_user_dst);
+		break;
+	default:
+		res = 0;
+		break;
+	}
+	if (file->ip->disk_info.type != INODE_TYPE_DIR) file->offset += res;
+	inode_unlock(file->ip);
+	return res;
 }
 
 /* 写入文件内容, 返回写入的字节数量 */
 uint32 file_write(file_t* file, uint32 len, uint64 src, bool is_user_src) {
-	return 0;
+	if (file->ip == NULL || !file->writbale) return 0;
+	uint32 res = 0;
+	inode_lock(file->ip);
+	switch (file->ip->disk_info.type) {
+	case INODE_TYPE_DATA:
+		res = inode_write_data(file->ip, file->offset, len, (void*)src, is_user_src);
+		break;
+	case INODE_TYPE_DIR:
+		res = 0;
+		break;
+	case INODE_TYPE_DIVICE:
+		res = device_write_data(file->ip->disk_info.major, len, src, is_user_src);
+		break;
+	default:
+		res = 0;
+		break;
+	}
+	if (file->ip->disk_info.type != INODE_TYPE_DIR) file->offset += res;
+	inode_unlock(file->ip);
+	return res;
 }
 
 /*
@@ -55,6 +124,22 @@ uint32 file_write(file_t* file, uint32 len, uint64 src, bool is_user_src) {
 	返回新的file->offset
 */
 uint32 file_lseek(file_t* file, uint32 lseek_offset, uint32 lseek_flag) {
+	if (file->ip == NULL) return 0;
+	inode_lock(file->ip);
+	switch (lseek_flag) {
+	case FILE_LSEEK_SET:
+		file->offset = lseek_offset;
+		break;
+	case FILE_LSEEK_ADD:
+		file->offset += lseek_offset;
+		break;
+	case FILE_LSEEK_SUB:
+		file->offset -= lseek_offset;
+		break;
+	}
+	if (file->offset > file->ip->disk_info.size) file->offset = file->ip->disk_info.size;
+	if (file->offset < 0) file->offset = 0;
+	inode_unlock(file->ip);
 	return file->offset;
 }
 
@@ -68,7 +153,17 @@ file_t* file_dup(file_t* file) {
 
 /* 获取文件参数, 成功返回0, 失败返回-1 */
 uint32 file_get_stat(file_t* file, uint64 user_dst) {
-	return -1;
+	if (file->ip == NULL) return -1;
+	inode_lock(file->ip);
+	file_stat_t stat;
+	stat.type = file->ip->disk_info.type;
+	stat.nlink = file->ip->disk_info.nlink;
+	stat.size = file->ip->disk_info.size;
+	stat.inode_num = file->ip->inode_num;
+	stat.offset = file->offset;
+	uvm_copyout(myproc()->pgtbl, user_dst, (uint64)&stat, sizeof(file_stat_t));
+	inode_unlock(file->ip);
+	return 0;
 }
 
 /* 基于superblock输出磁盘布局信息 (for debug) */
